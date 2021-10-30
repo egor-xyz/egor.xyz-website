0fd9ca028107fb059d818bb08c45909275bcaca4
Egor Stronhin <i@egor.xyz>
2021-10-30 15:36:20 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,89 @@
+function main() {
+  const canvas = document.querySelector('#c');
+  const renderer = new THREE.WebGLRenderer({ canvas });
+
+  const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 5000);
+  camera.rotation.y = 45 / 180 * Math.PI;
+  camera.position.x = 800;
+  camera.position.y = 100;
+  camera.position.z = 1000;
+
+  const controls = new THREE.OrbitControls(camera, canvas);
+  controls.target.set(0, 5, 0);
+  controls.update();
+
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x111213);
+
+  let hlight = new THREE.AmbientLight(0x404040, 100);
+  scene.add(hlight);
+
+  let directionalLight = new THREE.DirectionalLight(0xfffff, 10);
+  directionalLight.position.set(0, 1, 0);
+  directionalLight.castShadow = true;
+  scene.add(directionalLight);
+
+  let light = new THREE.PointLight(0xc4c4c4, 10);
+  light.position.set(0, 300, 500);
+  scene.add(light);
+
+  let light2 = new THREE.PointLight(0xc4c4c4, 10);
+  light2.position.set(500, 100, 0);
+  scene.add(light2);
+
+  let light3 = new THREE.PointLight(0xc4c4c4, 10);
+  light3.position.set(0, 100, -500);
+  // scene.add(light3);
+
+  let light4 = new THREE.PointLight(0xc4c4c4, 10);
+  light4.position.set(-500, 300, 500);
+  scene.add(light4);
+
+  {
+    const gltfLoader = new THREE.GLTFLoader();
+    gltfLoader.load('./scene.gltf', (gltf) => {
+      const root = gltf.scene;
+      scene.add(root);
+
+      // compute the box that contains all the stuff
+      // from root and below
+      const box = new THREE.Box3().setFromObject(root);
+      const boxSize = box.getSize(new THREE.Vector3()).length();
+      const boxCenter = box.getCenter(new THREE.Vector3());
+
+
+
+      // update the Trackball controls to handle the new size
+      controls.maxDistance = boxSize;
+      controls.target.copy(boxCenter);
+      controls.update();
+    });
+  }
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  function render() {
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
+}
+
+main();
